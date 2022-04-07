9604535a39c42cbb88cce6205f613b77d4043da8
Jakub Pietrucha <jakub.pietrucha89@gmail.com>
2022-04-07 16:32:39 +0200

    Update os.txt (mainly about pathlib)

--- regex_search.py
+++ regex_search.py
@@ -3,7 +3,7 @@ import re
 #metoda search znajduje PIERWSZY pasujacy ciag znakow
 
 phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
-mo = phoneNumRegex.search('My number is 415-555-4242.')
+mo = phoneNumRegex.search('My number is 415-555-4242.') # mo -> match object
 print('Phone number found: ' + mo.group())
 
 #grupowanie
