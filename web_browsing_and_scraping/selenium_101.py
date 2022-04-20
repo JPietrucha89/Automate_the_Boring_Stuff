f9f447976126968a30fbd213c0450cfa381e5f36
Jakub Pietrucha <jakub.pietrucha89@gmail.com>
2022-04-20 10:43:53 +0200

    Add selenium_101.py

---
+++ web_browsing_and_scraping/selenium_101.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+PATH='D:\Kodzenie\chromedriver.exe'
+
+browser= webdriver.Chrome(PATH)
+browser.get('https://aleplanszowki.pl/')
+
+# look for first element matching given css selector
+elem= browser.find_element_by_css_selector('#st_banner_block_21')
+##st_banner_block_21 to Gry Uszkodzone
+elem.click()
+
+# look for all elements matching given css selector
+elems=browser.find_elements_by_css_selector('p')
+print(len(elems))
+
+browser.back()
+
+# look for search inputBox and pass string argument to it, then click button Search
+elemSearch= browser.find_element_by_css_selector('#search_query_mobile_bar')
+elemSearch.send_keys('Rebelia')
+elemSearch.submit()
+
+browser.back()
+browser.forward()
+browser.refresh()
+browser.quit()
